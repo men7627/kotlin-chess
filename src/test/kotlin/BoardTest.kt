fe60be0ff88b5e0a027a9056b705be7f1c347f94
hyogeon <men1219@hanmail.net>
2020-03-16 21:46:27 +0900

    chore: junit5 dependency

---
+++ src/test/kotlin/BoardTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class BoardTest {
+    @Test
+    @DisplayName("체스판 초기화 테스트")
+    internal fun initialize() {
+        println("chore junit5")
+    }
+}
